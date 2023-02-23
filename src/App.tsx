6244da3e099f7214442a78c7628758c03614e699
Matheus Frej <matheusfrej@hotmail.com>
2023-02-23 13:28:20 -0300

    Routes and Layout configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Home } from './pages/Home'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
-function App() {
+export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Home />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
-
-export default App
